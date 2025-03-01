79f08455096b35ab54515a5137ff7d6694b05818
DGwebdes <alan.bilik8@gmail.com>
2025-03-01 18:02:34 +0000

    finally created the hamburger menu for small devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,62 @@
-import { Link } from 'react-router-dom'
-import { PropTypes } from 'prop-types'
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { PropTypes } from "prop-types";
 
-const Header = ( { handleHover } ) => {
-  return (
-    <header className="header">
-      <Link to={'/'}>
-        <img src="../../public/site-logo.svg" alt="nav-logo" width={60} height={60}
-          onMouseEnter={() => handleHover('home')}/>
-      </Link>
-        <ul className='navigation'>
-          <li className='books' onMouseEnter={() => handleHover('books')}>
-            <Link to={'./books'}>The Books</Link>
-          </li>
-          <li className='movies' onMouseEnter={() => handleHover('movies')}>
-            <Link to={'/movies'}>The Movies</Link>
-          </li>
-          <li className='tolkien' onMouseEnter={() => handleHover('tolkien')}>
-            <Link to={'/tolkien'}>The Man</Link>
-          </li>
-          <li className='appendix' onMouseEnter={() => handleHover('appendix')}>
-            <Link to={'/appendix'}>Appendix</Link>
-          </li>
-        </ul>
-        <Link to={'/login'}>
-          <i className="fa-solid fa-circle-user"></i>
-        </Link>
-    </header>
-  )
-}
+const Header = ({ handleHover }) => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="header">
+            <Link to={"/"}>
+                <img
+                    src="../../public/site-logo.svg"
+                    alt="nav-logo"
+                    width={60}
+                    height={60}
+                    onMouseEnter={() => handleHover("home")}
+                />
+            </Link>
+
+            {/* Hamburger Menu Icon */}
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            {/* Navigation Links */}
+            <ul className={`navigation ${menuOpen ? "active" : ""}`}>
+                <li className="books" onMouseEnter={() => handleHover("books")}>
+                    <Link to={"./books"}>The Books</Link>
+                </li>
+                <li
+                    className="movies"
+                    onMouseEnter={() => handleHover("movies")}
+                >
+                    <Link to={"/movies"}>The Movies</Link>
+                </li>
+                <li
+                    className="tolkien"
+                    onMouseEnter={() => handleHover("tolkien")}
+                >
+                    <Link to={"/tolkien"}>The Man</Link>
+                </li>
+                <li
+                    className="appendix"
+                    onMouseEnter={() => handleHover("appendix")}
+                >
+                    <Link to={"/appendix"}>Appendix</Link>
+                </li>
+            </ul>
+            <Link to={"/login"}>
+                <i className="fa-solid fa-circle-user"></i>
+            </Link>
+        </header>
+    );
+};
 
 Header.propTypes = {
-  handleHover: PropTypes.func,
-}
+    handleHover: PropTypes.func,
+};
 
-export default Header
+export default Header;
